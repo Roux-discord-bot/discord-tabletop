240d3165ad2fa6cffc4e0c2696385a80d087be5d
Roux <thraverdy@gmail.com>
2020-08-16 23:43:25 +0200

    needed for tests to pass

---
+++ src/core/services/discord-config.ts
@@ -0,0 +1,9 @@
+import IDiscordConfig from "../interfaces/discord-config-interface";
+
+const config: IDiscordConfig = {
+	getDiscordToken(): string {
+		return `TOKEN`;
+	},
+};
+
+export default config;

576d576a7baf1e3267516d0bbb721d48ba882f2f
Roux <thraverdy@gmail.com>
2020-08-16 16:26:12 +0200

    utils:logger for log/warn/error

---
+++ src/core/utils/logger.ts
@@ -0,0 +1,44 @@
+/* eslint-disable no-console */
+import _ from "lodash";
+import chalk from "chalk";
+
+export function log(message: string, options?: chalk.Chalk): void {
+	if (!_.isNil(options)) console.log(options`${message}`);
+	else console.log(`[${chalk.blueBright`INFO`}] ${message}`);
+}
+
+export function logEvent(event: string, message: string): void {
+	console.log(`${chalk.bgBlue.bold.black`[${event}]`} ${message}`);
+}
+
+export function warn(message: string, options?: chalk.Chalk): void {
+	if (!_.isNil(options)) console.log(options`${message}`);
+	else console.log(chalk.bold.yellow`[WARN] ${message}`);
+}
+
+export function warnEvent(event: string, message: string): void {
+	console.log(
+		`${chalk.bgYellow.bold.black`[${event}]`} ${chalk.bold.yellow`${message}`}`
+	);
+}
+
+export function error(message: string, options?: chalk.Chalk): void {
+	if (!_.isNil(options)) console.log(options`${message}`);
+	else console.log(chalk.bgRed.bold.black`[ERROR] ${message}`);
+}
+
+export function errorEvent(event: string, message: string): void {
+	console.log(
+		`${chalk.bgRed.bold.black`[${event}]`}${chalk.bgRed.bold
+			.black` ${message}`}`
+	);
+}
+
+export default {
+	log,
+	logEvent,
+	warn,
+	warnEvent,
+	error,
+	errorEvent,
+};

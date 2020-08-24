95ea36eba1d489997a145035432bcfb48461182d
Roux <thraverdy@gmail.com>
2020-08-24 20:29:33 +0200

    on ready event using eventhandler

---
+++ src/events/discord-event-ready.ts
@@ -0,0 +1,15 @@
+import { LoggerService } from "../utils/logger/logger-service";
+import { DiscordEventHandler } from "../core/discord/features/discord-event-handler";
+
+export class DiscordEventReady extends DiscordEventHandler {
+	constructor() {
+		super(`ready`);
+	}
+
+	public handleEvent(): void {
+		LoggerService.getInstance().info({
+			context: `Event - Ready`,
+			message: `Bot is ready to go !`,
+		});
+	}
+}

4f155e3a056d04f60efd7c9fe1c95ecef7b46a79
Roux <thraverdy@gmail.com>
2020-08-28 12:31:29 +0200

    event-emitter-service now done

---
+++ src/core/discord/services/discord-event-emitter-service.ts
@@ -0,0 +1,48 @@
+import { ClientEvents } from "discord.js";
+import _ from "lodash";
+import { LoggerService } from "../../utils/logger/logger-service";
+import { DiscordClientService } from "./discord-client-service";
+
+export class DiscordEventEmitterService {
+	private static _instance: DiscordEventEmitterService;
+
+	public static getInstance(): DiscordEventEmitterService {
+		if (_.isNil(DiscordEventEmitterService._instance))
+			DiscordEventEmitterService._instance = new DiscordEventEmitterService();
+
+		return DiscordEventEmitterService._instance;
+	}
+
+	public async emit<K extends keyof ClientEvents>(
+		event: K,
+		...args: ClientEvents[K]
+	): Promise<void>;
+
+	public async emit<S extends string | symbol>(
+		event: Exclude<S, keyof ClientEvents>,
+		...args: unknown[]
+	): Promise<void>;
+
+	public async emit(event: string, ...args: unknown[]): Promise<void> {
+		const client = DiscordClientService.getInstance().getClient();
+		if (!client) {
+			LoggerService.getInstance().error({
+				context: `DiscordEventEmitterService`,
+				message: `Got an undefined client while trying to emit '${event}'`,
+			});
+			return;
+		}
+		// Returns true if the event had listeners, false otherwise.
+		if (client.emit(event, args)) {
+			LoggerService.getInstance().info({
+				context: `DiscordEventEmitterService`,
+				message: `The event '${event}' was emitted`,
+			});
+		} else {
+			LoggerService.getInstance().warn({
+				context: `DiscordEventEmitterService`,
+				message: `The event '${event}' was emitted but has no listener`,
+			});
+		}
+	}
+}
